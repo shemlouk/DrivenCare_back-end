5d3bf635235651c98fd99abce2b10d33ae24ce63
samuel <shemlouk@gmail.com>
2023-03-29 20:38:48 -0300

    feat: add users controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,13 @@
+import userServices from "../services/userServices";
+
+const create = async (req, res, next) => {
+  const data = req.body;
+  try {
+    await userServices.create(data);
+    res.sendStatus(201);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { create };
