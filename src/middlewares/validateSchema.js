0a8d3b04edaf9c70ef67ed3e460504c8eea32d28
samuel <shemlouk@gmail.com>
2023-03-29 19:46:20 -0300

    feat: add validate schemas middleware

---
+++ src/middlewares/validateSchema.js
@@ -0,0 +1,9 @@
+const validateSchema = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) return res.status(422).send(error.details);
+    next();
+  };
+};
+
+export default validateSchema;
