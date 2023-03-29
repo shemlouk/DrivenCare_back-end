df6a479a3b3ed570b3afdcfc9727083d812a9441
samuel <shemlouk@gmail.com>
2023-03-29 16:07:02 -0300

    feat: add user sign up schema

---
+++ src/schemas/index.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const userSignUpSchema = Joi.object({
+  name: Joi.string().pattern(new RegExp(`^[a-zA-Z\u00C0-\u00FF ]{2,50}$`)),
+  email: Joi.string().email(),
+  password: Joi.string().pattern(new RegExp(`^[\w\W]{8,16}$`)),
+})
+  .options({ presence: "required" })
+  .required();
