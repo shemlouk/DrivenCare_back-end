440b8527021c63115985c59e5cf70d0d1a5f30fe
samuel <shemlouk@gmail.com>
2023-03-29 20:36:34 -0300

    feat: add users services file

---
+++ src/services/userServices.js
@@ -0,0 +1,15 @@
+import userRepository from "../repositories/userRepository";
+import bcrypt from "bcrypt";
+
+const create = async ({ name, email, password }) => {
+  const { rowCount: isDuplicate } = await userRepository.findByEmail(email);
+  if (isDuplicate) {
+    const error = new Error("Email already signed up");
+    error.statusCode = 409;
+    throw error;
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
+  await userRepository.create({ name, email, password: hashPassword });
+};
+
+export default { create };
