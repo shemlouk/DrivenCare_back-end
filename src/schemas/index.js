87d88ffaa7cb992e00ed9dbefc20e8012d4d278f
samuel <shemlouk@gmail.com>
2023-03-29 19:43:32 -0300

    chore: change user sign up schema name

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-export const userSignUpSchema = Joi.object({
+export const userSignUp = Joi.object({
   name: Joi.string().pattern(new RegExp(`^[a-zA-Z\u00C0-\u00FF ]{2,50}$`)),
   email: Joi.string().email(),
   password: Joi.string().pattern(new RegExp(`^[\w\W]{8,16}$`)),
