940891a145eb017aca397c0625f6a762a1742088
samuel <shemlouk@gmail.com>
2023-03-29 21:51:31 -0300

    fix: remove schema password pattern

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const userSignUp = Joi.object({
   name: Joi.string().pattern(new RegExp(`^[a-zA-Z\u00C0-\u00FF ]{2,50}$`)),
   email: Joi.string().email(),
-  password: Joi.string().pattern(new RegExp(`^[\w\W]{8,16}$`)),
+  password: Joi.string().min(8).max(16),
 })
   .options({ presence: "required" })
   .required();
