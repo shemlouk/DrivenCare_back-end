fdc6df6d334fd849c9f061def446820cbf5a7c37
samuel <shemlouk@gmail.com>
2023-03-30 10:08:45 -0300

    fix: remove alphanum from schema

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -27,9 +27,9 @@ export const signIn = Joi.object({
 export const doctorOffice = Joi.object({
   city: Joi.string().pattern(new RegExp(`^[a-zA-Z\u00c0-\u00FF ]{3,30}$`)),
   state: Joi.string().pattern(new RegExp(`^[A-Z]{2}$`)),
-  street: Joi.string().min(5).alphanum(),
+  street: Joi.string().min(5),
   zip_code: Joi.string().pattern(new RegExp(`^[0-9]{8}$`)),
-  neighborhood: Joi.string().min(3).alphanum(),
+  neighborhood: Joi.string().min(3),
   address_number: Joi.string().max(6).alphanum(),
 })
   .options({ presence: "required" })
