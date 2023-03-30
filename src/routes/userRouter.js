3c1a365b0ac97430731554eb71613186a9e4aa6b
samuel <shemlouk@gmail.com>
2023-03-29 23:43:57 -0300

    feat: add sign in to users router

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,11 @@
 import userController from "../controllers/userController.js";
 import validateSchema from "../middlewares/validateSchema.js";
-import { userSignUp } from "../schemas/index.js";
+import { userSignUp, signIn } from "../schemas/index.js";
 import { Router } from "express";
 
 const userRouter = Router();
 
 userRouter.post("/signup", validateSchema(userSignUp), userController.create);
+userRouter.post("/signin", validateSchema(signIn), userController.signIn);
 
 export default userRouter;
